b0cc0aa6850743709cf20b297a369275cf35d8f3
baohnq <104848654+baohnq@users.noreply.github.com>
2022-05-18 17:41:15 +0700

    comment

--- trust-region/func_optimization.py
+++ trust-region/func_optimization.py
@@ -1,10 +1,10 @@
 from utils import*
-def rosenbrock(x, b=5):
+def rosenbrock(x, b=5):   # hàm rosenbrock
     return (x[0] - 1)**2 + b * (x[1] - x[0]**2)**2
 
-def rosenbrock_grad(x, b=5):
+def rosenbrock_grad(x, b=5):  #đạo hàm bậc 1 (gradient)
     return np.array([2 * (x[0] - 1) - 4 * b * (x[1] - x[0]**2) * x[0], 2 * b * (x[1] - x[0]**2)])
 
-def rosenbrock_hess(x, b=5):
+def rosenbrock_hess(x, b=5): # đạo hầm bậc 2 (Hessian)
     return np.array([[2 - 4 * b * x[1] + 12 * b * x[0]**2, -4 * b * x[0]]\
                      ,[-4 * b * x[0], 2 * b]])
--- trust-region/method.py
+++ trust-region/method.py
@@ -2,8 +2,11 @@ from utils import*
 
 def solve_trust_region_subproblem(f_grad, H, x0, delta):
     x = cp.Variable(x0.shape)
-    objective = cp.Minimize((x - x0).T @ f_grad(x0) + cp.quad_form(x - x0, H(x0)) * 0.5)
+    #tối tiểu
+    objective = cp.Minimize((x - x0).T @ f_grad(x0) + cp.quad_form(x - x0, H(x0)) * 0.5) 
+    # ràng buộc
     constraints = [cp.norm(x - x0) <= delta]
+    # giải
     problem = cp.Problem(objective, constraints)
     problem.solve()
     return x.value, problem.value
@@ -13,13 +16,19 @@ def trust_region_descent(f, f_grad, H, x, k_max, eta1=0.4, eta2=0.6, gamma1=0.5,
     
     y = f(x)
     for k in tqdm(range(k_max)):
-        x_, y_ = solve_trust_region_subproblem(f_grad, H, x, delta)
-        r = (y - f(x_)) / (y - y_)
-        if r < eta1:
+        # tối tiểu cái f^ để thu được bước x' tiếp
+        # và giá trị của f^(x'), giá trị này thì lưu vào y_
+        x_, y_ = solve_trust_region_subproblem(f_grad, H, x, delta)     
+        r = (y - f(x_)) / (y - y_)  # tính cái tỷ số
+
+        # cập nhật vùng tin cậy dựa theo cái tỷ số đó
+        if r < eta1:  
+            # dưới ngưỡng thì thhu hẹp bán kính lại
             delta *= gamma1
         else:
+            # ngược lại, bước x' dự đoán được chấp thuận, cập nhật nó
             x, y = x_, y_
-            if r > eta2:
+            if r > eta2: # mở rộng cái vùng tin cậy lên
                 delta *= gamma2
         pts.append(x_)
         rads.append(delta)
